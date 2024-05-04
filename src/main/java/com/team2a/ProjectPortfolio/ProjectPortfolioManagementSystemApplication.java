f73a0c66d68daf3c144bbb93fdd2575d5fab01b6
PaulAnton <p.anton@student.tudelft.nl>
2024-05-04 17:24:30 +0200

    Checkstyle

--- src/main/java/com/team2a/ProjectPortfolio/ProjectPortfolioManagementSystemApplication.java
+++ src/main/java/com/team2a/ProjectPortfolio/ProjectPortfolioManagementSystemApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjectPortfolioManagementSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjectPortfolioManagementSystemApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectPortfolioManagementSystemApplication.class, args);
+    }
 
 }
