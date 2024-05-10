ec762747eea0f0f499b2f975182c727a9e058cfd
Filip-Andrei Cirtog <f.a.cirtog@student.tudelft.nl>
2024-05-10 12:55:10 +0200

    Created a controller and a service for this getting all the collaborators of a specific project. Also, tested and wrote Javadoc. Fixed pipeline.
    #Closes 39

--- src/main/java/com/team2a/ProjectPortfolio/Repositories/ProjectsToCollaboratorsRepository.java
+++ src/main/java/com/team2a/ProjectPortfolio/Repositories/ProjectsToCollaboratorsRepository.java
@@ -8,6 +8,5 @@ import java.util.List;
 import java.util.UUID;
 @Repository
 public interface ProjectsToCollaboratorsRepository extends JpaRepository<ProjectsToCollaborators, UUID> {
-    @SuppressWarnings("checkstyle:MethodName")
     List<ProjectsToCollaborators> findAllByProjectProjectId (UUID projectId);
 }
