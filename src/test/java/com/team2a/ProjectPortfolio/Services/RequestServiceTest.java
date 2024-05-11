daf04dc38763b273ec7f981ecacedc83e9a30b74
lsionescu <ionescu-8@student.tudelft.nl>
2024-05-11 23:15:02 +0200

    added endpoint for getting requests for a specific project, along with tests

--- src/test/java/com/team2a/ProjectPortfolio/Services/RequestServiceTest.java
+++ src/test/java/com/team2a/ProjectPortfolio/Services/RequestServiceTest.java
@@ -1,9 +1,11 @@
 package com.team2a.ProjectPortfolio.Services;
 
 import com.team2a.ProjectPortfolio.Commons.Account;
+import com.team2a.ProjectPortfolio.Commons.Project;
 import com.team2a.ProjectPortfolio.Commons.Request;
-import com.team2a.ProjectPortfolio.Exceptions.NotFoundException;
+import com.team2a.ProjectPortfolio.CustomExceptions.NotFoundException;
 import com.team2a.ProjectPortfolio.Repositories.AccountRepository;
+import com.team2a.ProjectPortfolio.Repositories.ProjectRepository;
 import com.team2a.ProjectPortfolio.Repositories.RequestRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -26,26 +28,30 @@ class RequestServiceTest {
     private AccountRepository accountRepository;
     @Mock
     private RequestRepository requestRepository;
+    @Mock
+    private ProjectRepository projectRepository;
 
     @BeforeEach
     void setup() {
         sut = new RequestService();
         accountRepository = Mockito.mock(AccountRepository.class);
         requestRepository = Mockito.mock(RequestRepository.class);
+        projectRepository = Mockito.mock(ProjectRepository.class);
         sut.setAccountRepository(accountRepository);
         sut.setRequestRepository(requestRepository);
+        sut.setProjectRepository(projectRepository);
     }
 
     @Test
     void testGetRequestsForUserEmptyUsername() {
-        assertThrows(NotFoundException.class, () -> {sut.getRequestsForUser(null);});
+        assertThrows(NotFoundException.class, () -> sut.getRequestsForUser(null));
     }
 
     @Test
     void testGetRequestForUserUserNotFound() {
         when(accountRepository.findAll()).thenReturn(List.of(new Account("uname",
                 "Name", "pw", true, false, new ArrayList<>())));
-        assertThrows(NotFoundException.class, () -> {sut.getRequestsForUser("Name");});
+        assertThrows(NotFoundException.class, () -> sut.getRequestsForUser("Name"));
     }
 
     @Test
@@ -67,7 +73,60 @@ class RequestServiceTest {
         assertEquals(sut.getRequests(), List.of(r));
     }
 
+    @Test
+    void testGetRequestsForProjectNull () {
+        assertThrows(NotFoundException.class, () -> sut.getRequestsForProject(null));
+    }
 
+    @Test
+    void testGetRequestsForProjectNotFound () {
+        UUID id1 = UUID.randomUUID();
+        UUID id2 = UUID.randomUUID();
+
+        while(id2.equals(id1)){
+            id2 = UUID.randomUUID();
+        }
+
+        Project p = new Project("title", "desc", "bibtex", false);
+        p.setProjectId(id1);
+        when(projectRepository.findAll()).thenReturn(List.of(p));
+        UUID finalId = id2;
+        assertThrows(NotFoundException.class, () -> sut.getRequestsForProject(finalId));
+    }
+
+    @Test
+    void testGetRequestsForProjectEmptyList () {
+        UUID id1 = UUID.randomUUID();
+        UUID id2 = UUID.randomUUID();
+
+        while(id2.equals(id1)){
+            id2 = UUID.randomUUID();
+        }
+
+        Project p = new Project("title", "desc", "bibtex", false);
+        p.setProjectId(id1);
+        p.setRequests(new ArrayList<>());
+        when(projectRepository.findAll()).thenReturn(List.of(p));
+        assertEquals(sut.getRequestsForProject(id1), new ArrayList<>());
+    }
+
+    @Test
+    void testGetRequestsForProjectNonEmptyList () {
+        UUID id1 = UUID.randomUUID();
+        UUID id2 = UUID.randomUUID();
+
+        while(id2.equals(id1)){
+            id2 = UUID.randomUUID();
+        }
+
+        Project p = new Project("title", "desc", "bibtex", false);
+        p.setProjectId(id1);
+        Request r = new Request(UUID.randomUUID(), "title", "description", "bibtex", false);
+
+        p.setRequests(List.of(r));
+        when(projectRepository.findAll()).thenReturn(List.of(p));
+        assertEquals(sut.getRequestsForProject(id1), List.of(r));
+    }
 
 
 
