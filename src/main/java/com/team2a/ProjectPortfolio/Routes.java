7b42daf75bced5a4adec6d042082985621090087
Filip-Andrei Cirtog <f.a.cirtog@student.tudelft.nl>
2024-05-11 15:58:49 +0200

    - [x] Created a method in the controller - [x] Created a method in the service - [x] Tested and wrote Javadoc for the service and the controller
    References #62
    Closes #63

--- src/main/java/com/team2a/ProjectPortfolio/Routes.java
+++ src/main/java/com/team2a/ProjectPortfolio/Routes.java
@@ -2,8 +2,9 @@ package com.team2a.ProjectPortfolio;
 
 
 public class Routes {
-    public final static String COLLABORATORS = "/collaborator";
+    public final static String COLLABORATOR = "/collaborator";
     public final static String PROJECT = "/project";
     public final static String REQUESTS = "/request";
+    public final static String LINK = "/link";
 
 }
