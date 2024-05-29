6e49508b1fe7c05928e8ba616f0b51d37ac34955
lsionescu <ionescu-8@student.tudelft.nl>
2024-05-29 02:11:23 +0200

    fixed conflicts with main

--- src/main/java/com/team2a/ProjectPortfolio/Commons/Project.java
+++ src/main/java/com/team2a/ProjectPortfolio/Commons/Project.java
@@ -1,5 +1,6 @@
 package com.team2a.ProjectPortfolio.Commons;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
